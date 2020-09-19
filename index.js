f3f35e51d951c838b2ac533f485c99b3f5676d51
Layi <simplesemaster@gmail.com>
2020-09-20 03:16:19 +0800

    myApp

--- index.js
+++ index.js
@@ -39,156 +39,41 @@ bot.on('message', function(event) {
                     }else if(data == -9){                    
                         event.reply('執行錯誤');
                     }else{
-                        event.reply([
-                            {"type": "bubble",
-                            "hero": {
-                              "type": "image",
-                              "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png",
-                              "size": "full",
-                              "aspectRatio": "20:13",
-                              "aspectMode": "cover",
-                              "action": {
-                                "type": "uri",
-                                "uri": "http://linecorp.com/"
-                              }
-                            },
-                            "body": {
-                              "type": "box",
-                              "layout": "vertical",
-                              "contents": [
-                                {
-                                  "type": "text",
-                                  "text": "Brown Cafe",
-                                  "weight": "bold",
-                                  "size": "xl"
-                                },
-                                {
-                                  "type": "box",
-                                  "layout": "baseline",
-                                  "margin": "md",
-                                  "contents": [
-                                    {
-                                      "type": "icon",
-                                      "size": "sm",
-                                      "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-                                    },
-                                    {
-                                      "type": "icon",
-                                      "size": "sm",
-                                      "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-                                    },
-                                    {
-                                      "type": "icon",
-                                      "size": "sm",
-                                      "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-                                    },
-                                    {
-                                      "type": "icon",
-                                      "size": "sm",
-                                      "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-                                    },
-                                    {
-                                      "type": "icon",
-                                      "size": "sm",
-                                      "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gray_star_28.png"
-                                    },
-                                    {
-                                      "type": "text",
-                                      "text": "4.0",
-                                      "size": "sm",
-                                      "color": "#999999",
-                                      "margin": "md",
-                                      "flex": 0
-                                    }
-                                  ]
-                                },
-                                {
-                                  "type": "box",
-                                  "layout": "vertical",
-                                  "margin": "lg",
-                                  "spacing": "sm",
-                                  "contents": [
-                                    {
-                                      "type": "box",
-                                      "layout": "baseline",
-                                      "spacing": "sm",
-                                      "contents": [
-                                        {
-                                          "type": "text",
-                                          "text": "Place",
-                                          "color": "#aaaaaa",
-                                          "size": "sm",
-                                          "flex": 1
-                                        },
-                                        {
-                                          "type": "text",
-                                          "text": "Miraina Tower, 4-1-6 Shinjuku, Tokyo",
-                                          "wrap": true,
-                                          "color": "#666666",
-                                          "size": "sm",
-                                          "flex": 5
-                                        }
-                                      ]
-                                    },
-                                    {
-                                      "type": "box",
-                                      "layout": "baseline",
-                                      "spacing": "sm",
-                                      "contents": [
-                                        {
-                                          "type": "text",
-                                          "text": "Time",
-                                          "color": "#aaaaaa",
-                                          "size": "sm",
-                                          "flex": 1
-                                        },
-                                        {
-                                          "type": "text",
-                                          "text": "10:00 - 23:00",
-                                          "wrap": true,
-                                          "color": "#666666",
-                                          "size": "sm",
-                                          "flex": 5
-                                        }
-                                      ]
-                                    }
-                                  ]
-                                }
-                              ]
-                            },
-                            "footer": {
-                              "type": "box",
-                              "layout": "vertical",
-                              "spacing": "sm",
-                              "contents": [
-                                {
-                                  "type": "button",
-                                  "style": "link",
-                                  "height": "sm",
-                                  "action": {
+                        event.reply({
+                            "type": "template",
+                            "altText": "這是按鈕樣板",
+                            "template": {
+                                "type": "buttons",
+                                "thumbnailImageUrl": "https://tomlin-app-1.herokuapp.com/imgs/p01.jpg",
+                                "imageAspectRatio": "rectangle",
+                                "imageSize": "cover",
+                                "imageBackgroundColor": "#FFFFFF",
+                                "title": "梵谷-星夜",
+                                "text": "荷蘭後印象派畫家文森特·梵谷於1890年在法國聖雷米的一家精神病院裏創作的一幅著名油畫",
+                                "defaultAction": {
                                     "type": "uri",
-                                    "label": "CALL",
-                                    "uri": "https://linecorp.com"
-                                  }
+                                    "label": "詳細資料",
+                                    "uri": "https://zh.wikipedia.org/wiki/%E6%98%9F%E5%A4%9C"
                                 },
-                                {
-                                  "type": "button",
-                                  "style": "link",
-                                  "height": "sm",
-                                  "action": {
-                                    "type": "uri",
-                                    "label": "WEBSITE",
-                                    "uri": "https://linecorp.com"
-                                  }
-                                },
-                                {
-                                  "type": "spacer",
-                                  "size": "sm"
-                                }
-                              ],
-                              "flex": 0
-                            }},]
-                        );  
+                                "actions": [
+                                    {
+                                      "type": "postback",
+                                      "label": "買了",
+                                      "data": "action=buy&itemid=123"
+                                    },
+                                    {
+                                      "type": "postback",
+                                      "label": "加入購物車",
+                                      "data": "action=add&itemid=123"
+                                    },
+                                    {
+                                      "type": "uri",
+                                      "label": "詳細資料",
+                                      "uri": "https://zh.wikipedia.org/wiki/%E6%98%9F%E5%A4%9C"
+                                    }
+                                ]
+                            }
+                          });  
                     }  
                 })
             }else if (event.message.text.includes("查詢")) {
