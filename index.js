8dcf28a0fac8b76d7c175960b36a3dab776cebc9
Layi <simplesemaster@gmail.com>
2020-09-21 22:24:11 +0800

    myApp

--- index.js
+++ index.js
@@ -68,6 +68,31 @@ bot.on('message', function(event) {
                                       "label": "星期三",
                                       "text": "星期三"
                                     },
+                                    {
+                                        "type": "message",
+                                        "label": "星期四",
+                                        "text": "星期四"
+                                    },
+                                    {
+                                        "type": "message",
+                                        "label": "星期五",
+                                        "text": "星期五"
+                                    },
+                                    {
+                                        "type": "message",
+                                        "label": "星期六",
+                                        "text": "星期六"
+                                    },
+                                    {
+                                        "type": "message",
+                                        "label": "星期日",
+                                        "text": "星期日"
+                                    },
+                                    {
+                                        "type": "message",
+                                        "label": "列出全部",
+                                        "text": "列出全部"
+                                    },
                                 ]
                             }
                           });  
