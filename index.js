1af9a210376d5e4a746739d5d1b8b9ed71261f9c
Layi <simplesemaster@gmail.com>
2020-09-21 18:02:02 +0800

    myApp

--- index.js
+++ index.js
@@ -41,20 +41,10 @@ bot.on('message', function(event) {
                     }else{
                         event.reply({
                             "type": "template",
-                            "altText": "這是按鈕樣板",
+                            "altText": "選擇星期幾",
                             "template": {
                                 "type": "buttons",
-                                "thumbnailImageUrl": "https://tomlin-app-1.herokuapp.com/imgs/p01.jpg",
-                                "imageAspectRatio": "rectangle",
-                                "imageSize": "cover",
-                                "imageBackgroundColor": "#FFFFFF",
-                                "title": "梵谷-星夜",
-                                "text": "荷蘭後印象派畫家文森特·梵谷於1890年在法國聖雷米的一家精神病院裏創作的一幅著名油畫",
-                                "defaultAction": {
-                                    "type": "uri",
-                                    "label": "詳細資料",
-                                    "uri": "https://zh.wikipedia.org/wiki/%E6%98%9F%E5%A4%9C"
-                                },
+                                "title": "要查詢老師星期幾的課表呢？",
                                 "actions": [
                                     {
                                       "type": "postback",
