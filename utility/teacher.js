7e3f02ad1a89cd1706679ccff6a7409f415410e9
Layi <simplesemaster@gmail.com>
2020-09-28 21:14:04 +0800

    myApp

--- utility/teacher.js
+++ utility/teacher.js
@@ -16,19 +16,16 @@ var fetchTeacher = async function(teachername){
             if(data.rows.length > 0){
                 for (var i = 0; i < data.rows.length; i++) {
                     result = data.rows[i];
-                    return result;
                 } //學生資料(物件)
             }else{
                 result = -1;  //找不到資料
-                return result;
             }    
         }, (error) => {
             result = -9;  //執行錯誤
-            return result;
         });
 
     //回傳執行結果
-      
+    return result;  
 }
 //------------------------------------------
 
