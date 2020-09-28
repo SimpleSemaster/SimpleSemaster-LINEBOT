19ef7562e1ed99145b4daa5c6a2773a7c1a531bf
Layi <simplesemaster@gmail.com>
2020-09-28 19:06:34 +0800

    myApp

--- utility/teacher.js
+++ utility/teacher.js
@@ -14,7 +14,9 @@ var fetchTeacher = async function(teachername){
     await query('select * from teacher,course where teachername = $1', [teachername])
         .then((data) => {
             if(data.rows.length > 0){
-                result = data.rows;  //學生資料(物件)
+                for (var i = 0; i < data.rows.length; i++) {
+                    result=data.rows[i];
+                  } //學生資料(物件)
             }else{
                 result = -1;  //找不到資料
             }    
