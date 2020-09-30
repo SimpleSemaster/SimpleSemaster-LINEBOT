1889dd591dcc45f7d2f5dc5ea7e7fdc622111c29
Layi <simplesemaster@gmail.com>
2020-09-30 21:22:55 +0800

    myApp

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ var express = require('express');
 const teacher = require('./utility/teacher');
 const course = require('./utility/course');
 const whichday = require('./utility/whichday');
-
+function addMinutes(time, minutes) {
+    return new Date(time.getTime() + minutes*60000);
+}
 
 //----------------------------------------
 // 填入自己在Line Developers的channel值
@@ -48,7 +50,7 @@ bot.on('message', function(event) {
                         }else{
                             msg = msg + '\n';
                         }
-                        msg = msg + "課程名稱：" + item.coursename + "\n星期" + item.whichday + "\n從第" + item.starttime + "節課("+ item.periodstarttime + ")到第" + item.endtime + "節課("+ item.periodstarttime + 1000 * 60 * 50 * (item.endtime - item.starttime) + ")\n";
+                        msg = msg + "課程名稱：" + item.coursename + "\n星期" + item.whichday + "\n從第" + item.starttime + "節課("+ item.periodstarttime + ")到第" + item.endtime + "節課("+ addMinutes(item.periodstarttime,50*(item.endtime - item.starttime) + ")\n";
                         });
                     event.reply({type:'text', text: msg + "\nhttp://ntcbadm.ntub.edu.tw/pub/Cur_Teachers.aspx"});
                         /*event.reply('要查詢星期幾呢？');
