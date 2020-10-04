759c195167f25b1306ecdac4e0284d6e86d28bac
Layi <simplesemaster@gmail.com>
2020-10-05 03:24:06 +0800

    myApp

--- utility/course.js
+++ utility/course.js
@@ -11,7 +11,7 @@ var fetchCourse = async function(coursename){
     let result;  
 
     //讀取資料庫
-    await query('select *,teachername,* from course,teacher,period where coursename = $1 and course.teacherno = teacher.teacherno', [coursename])
+    await query('select teachername,*,*,* from teacher as a,course as b,periodstarttime as c,periodendtime as d where coursename = $1 and b.teacherno = a.teacherno and b.starttime = c.periodstartno and b.daynight = c.daynight and b.endtime = d.periodendno and b.daynight = d.daynight', [coursename])
         .then((data) => {
             if(data.rows.length > 0){
                 result = data.rows[0];  //學生資料(物件)
