9a1b11dc293b9c04bdaadda4750125a26c7a1e26
Layi <simplesemaster@gmail.com>
2020-10-12 01:47:03 +0800

    myApp

--- utility/credits.js
+++ utility/credits.js
@@ -6,7 +6,7 @@ const query = require('./asyncDB');
 //------------------------------------------
 // 由學號查詢學生資料
 //------------------------------------------
-var fetchCredits = async function(year){
+var fetchCredits = async function(year,schoolsys){
     //存放結果
     let result;  
 
