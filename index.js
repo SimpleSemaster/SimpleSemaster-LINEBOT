371fa80af3b84e4daa8d803e5d4caf164a780df9
Layi <simplesemaster@gmail.com>
2020-11-02 23:34:53 +0800

    myApp

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ var express = require('express');
 const teacher = require('./utility/teacher');
 const course = require('./utility/course');
 const credits = require('./utility/credits');
-
+const dialogflow = require('dialogflow');
+const uuid = require('uuid');
 
 //----------------------------------------
 // 填入自己在Line Developers的channel值
@@ -19,6 +20,38 @@ var bot = linebot({
     channelAccessToken: 'qiYcWUMLVg5qA8FLI192DseDohHH/K473s3XK0jjFgkSv1Gx1sxFhFXGAcjCyDnXL/uN2rTsvPmESiaJDBZJ1uWx6eND/JhjL/pvV49jvUfsmsaJFaVGbsd4EoVDTRp8c74x4KeQ6rqKyASyAjjHfgdB04t89/1O/w1cDnyilFU='
 });
 
+
+async function runSample(projectId = 'newagent-khstto') {
+    // A unique identifier for the given session
+    const sessionId = uuid.v4();
+   
+    // Create a new session
+    const sessionClient = new dialogflow.SessionsClient();
+    const sessionPath = sessionClient.sessionPath(projectId, sessionId);
+   
+    // The text query request.
+    const request = {
+      session: sessionPath,
+      queryInput: {
+        text: {
+          // The query to send to the dialogflow agent
+          text: 'hello',
+          // The language used by the client (en-US)
+          languageCode: 'zh-TW',
+        },
+      },
+    };
+    const responses = await sessionClient.detectIntent(request);
+    console.log('Detected intent');
+    const result = responses[0].queryResult;
+    console.log(`  Query: ${result.queryText}`);
+    console.log(`  Response: ${result.fulfillmentText}`);
+    if (result.intent) {
+        console.log(`  Intent: ${result.intent.displayName}`);
+    } else {
+        console.log(`  No intent matched.`);
+    }
+}
 //--------------------------------
 // 機器人接受訊息的處理
 //--------------------------------
