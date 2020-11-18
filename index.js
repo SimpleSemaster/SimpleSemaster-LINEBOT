6c45c6b4fedc6387a9e7bc97a1e75e6a49b0293e
Layi <simplesemaster@gmail.com>
2020-11-18 23:07:36 +0800

    myApp

--- index.js
+++ index.js
@@ -3,13 +3,15 @@
 //----------------------------------------
 var linebot = require('linebot');
 var express = require('express');
+const { WebhookClient } = require('dialogflow-fulfillment')
+const app = express()
 
 //增加引用函式
 const teacher = require('./utility/teacher');
 const course = require('./utility/course');
 const credits = require('./utility/credits');
 
-
+/*
 //----------------------------------------
 // 填入自己在Line Developers的channel值
 //----------------------------------------
@@ -17,20 +19,42 @@ var bot = linebot({
     channelId: '1654206823',
     channelSecret: '13539dd66169e8c2a548d9f206a69851',
     channelAccessToken: 'qiYcWUMLVg5qA8FLI192DseDohHH/K473s3XK0jjFgkSv1Gx1sxFhFXGAcjCyDnXL/uN2rTsvPmESiaJDBZJ1uWx6eND/JhjL/pvV49jvUfsmsaJFaVGbsd4EoVDTRp8c74x4KeQ6rqKyASyAjjHfgdB04t89/1O/w1cDnyilFU='
-});
+});*/
 
 //--------------------------------
 // 機器人接受訊息的處理
 //--------------------------------
-bot.on('message', function(event) {    
-    event.source.profile().then(
-        function (profile) {
-            //取得使用者資料
-            const userName = profile.displayName;
-            const userId = profile.userId;
+app.post('/dialogflow', express.json(), (req, res) => {    
+
+    const agent = new WebhookClient({ request: req, response: res })
+    
+    function SearchTeacher(agent) {
+        var teachername = req.body.queryResult.parameters.teachername;
+        console.log(teachername);
+        return teacher.fetchTeacher(teachername).then(data => {  
+            if (data == -1){
+                agent.add('找不到老師資料');
+            }else if(data == -9){                    
+                agent.add('執行錯誤');
+            }else{
+                let msg='';
+                let firstLine = true; 
+                data.forEach(item => {
+                if(firstLine){                            
+                    firstLine=false;
+                }else{
+                    msg = msg + '\n';
+                }
+                office = item.office;
+                msg = msg + "課程名稱：" + item.coursename + "\n星期" + item.whichday + "\n從第" + item.starttime + "節課("+ item.periodstarttime.slice(0,-3) + ")到第" + item.endtime + "節課("+ item.periodendtime.slice(0,-3) + ")\n教室："+item.classroom + "\n";
+                });
+                agent.add({type:'text', text: teachername+"老師的辦公室為："+office+"\n"+ msg +"\n詳細課表以學校官網為主：\nhttp://ntcbadm.ntub.edu.tw/pub/Cur_Teachers.aspx"});
+            }  
+        })
+    }
             
             
-            if (event.message.text.includes("查詢")&&event.message.text.includes("老師")) {
+           /* if (event.message.text.includes("查詢")&&event.message.text.includes("老師")) {
                 //使用者傳來的學號
                 const teachername = event.message.text.slice(2,-2);
                 //呼叫API取得學生資料
@@ -109,23 +133,14 @@ bot.on('message', function(event) {
                         );  
                     }  
                 })
-            }
-        }
-    );
-});
-
-//----------------------------------------
-// 建立一個網站應用程式app
-// 如果連接根目錄, 交給機器人處理
-//----------------------------------------
-const app = express();
-const linebotParser = bot.parser();
-app.post('/', linebotParser);
-
-//----------------------------------------
-// 可直接取用檔案的資料夾
-//----------------------------------------
-app.use(express.static('public'));
+            }*/
+            let intentMap = new Map();
+            //------------------------------------
+            intentMap.set('SearchTeacher', SearchTeacher);
+            //------------------------------------
+            agent.handleRequest(intentMap);
+        });
+   
 
 //----------------------------------------
 // 監聽3000埠號, 
