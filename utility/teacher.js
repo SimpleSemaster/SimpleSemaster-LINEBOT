44b8e7e17fb55ceb63924ff3f7e6bc60296a3fbf
Layi <simplesemaster@gmail.com>
2020-11-19 02:33:21 +0800

    myApp

--- utility/teacher.js
+++ utility/teacher.js
@@ -10,8 +10,8 @@ var fetchTeacher = async function(teachername){
     //存放結果
     var result;  
 
-    //讀取資料庫
-    await query('select teachername,*,*,* from teacher as a,course as b,periodstarttime as c,periodendtime as d where a.teachername = $1 and b.teacherno = a.teacherno and b.starttime = c.periodstartno and b.daynight = c.daynight and b.endtime = d.periodendno and b.daynight = d.daynight order by whichday', [teachername])
+    //讀取資料庫teachername,*,*,* from teacher as a,course as b,periodstarttime as c,periodendtime as d where a.teachername = $1 and b.teacherno = a.teacherno and b.starttime = c.periodstartno and b.daynight = c.daynight and b.endtime = d.periodendno and b.daynight = d.daynight order by whichday' 
+    await query('select * from teacher where teachername = $1', [teachername])
         .then((data) => {
             if(data.rows.length > 0){
                 result = data.rows;
