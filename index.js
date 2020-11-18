f158b6fcf5caf17614cae616ee4dcc9ee927930c
Layi <simplesemaster@gmail.com>
2020-11-19 01:54:12 +0800

    myApp

--- index.js
+++ index.js
@@ -40,13 +40,18 @@ app.post('/dialogflow', express.json(), (req, res) => {
 
     function SearchTeacher(agent) {
         var teachername = req.body.queryResult.parameters.teachername;
-        console.log(teachername);
+        
         return teacher.fetchTeacher(teachername).then(data => {  
             if (data == -1){
                 agent.add('找不到老師資料');
             }else if(data == -9){                    
                 agent.add('執行錯誤');
             }else{
+                agent.add('找到老師資料');
+                /*
+                for(var i=0; i<data.length; i++){
+                    agent.add(data[i].course + ":" + data[i].score + "分");
+                }  
                 let msg='';
                 let firstLine = true; 
                 data.forEach(item => {
@@ -59,7 +64,7 @@ app.post('/dialogflow', express.json(), (req, res) => {
                 msg = msg + "課程名稱：" + item.coursename + "\n星期" + item.whichday + "\n從第" + item.starttime + "節課("+ item.periodstarttime.slice(0,-3) + ")到第" + item.endtime + "節課("+ item.periodendtime.slice(0,-3) + ")\n教室："+item.classroom + "\n";
                 });
                 agent.add({type:'text', text: teachername+"老師的辦公室為："+office+"\n老師的課表如下："+ msg +"\n詳細課表以學校官網為主：\nhttp://ntcbadm.ntub.edu.tw/pub/Cur_Teachers.aspx"});
-            }  
+            */}  
         })
     }
             
