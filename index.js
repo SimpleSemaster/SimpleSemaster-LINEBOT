2e825e36350df4d068f0f286feaafa2cdb88058a
Layi <simplesemaster@gmail.com>
2020-11-21 18:15:41 +0800

    myApp

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const app = express();
 const teacher = require('./utility/teacher');
 const course = require('./utility/course');
 const credits = require('./utility/credits');
+const semcredits = require('./utility/semcredits');
 
 
 //--------------------------------
@@ -73,19 +74,33 @@ app.post('/dialogflow', express.json(), (req, res) => {
             }else if(data == -9){                    
                 agent.add('執行錯誤');
             }else{
-
                 agent.add("必修：學校每學期會安排必修課，不用自己選。\n安排的必修課都要過才能畢業。\n\n專業選修：在選課網上有標註【專選】之課程，\n需要自己選課並計算學分數，\n專選學分若未達指定門檻，\n即使總學分有超過畢業門檻也無法畢業。\n\n其餘課程(通識、一般選修等)與必修、專選學分相加後超過總學分即達成畢業門檻。\n\n"+
                     data.year + data.schoolsys +"的總學分數為" + data.minicre + "，\n其中專選學分數為" + data.minielecre +"。\n\n詳情可上教務處網站查詢：https://acad.ntub.edu.tw/p/412-1004-1718.php?Lang=zh-tw\n\n另有英語能力及專業證照之畢業門檻，請上以下網站查詢：https://imd.ntub.edu.tw/p/412-1043-1051.php"
                 )
             }
         })
     }
+
+    function SearchSemesterCredits() {
+        var schoolsys = req.body.queryResult.parameters.schoolsys;
+        var grade = req.body.queryResult.parameters.grade;
+        return semcredits.fetchSemCredits(schoolsys,grade).then(data => {  
+            if (data == -1){
+                agent.add('找不到學制資料');
+            }else if(data == -9){                    
+                agent.add('執行錯誤');
+            }else{
+                agent.add(schoolsys+grade+"年級的最低選課學分為："+ minsemcre + "\n最高選課學分為："+maxsemcre);
+            };
+        })
+    }  
     let intentMap = new Map();
     //------------------------------------
     intentMap.set('SearchTeacher', SearchTeacher)
     intentMap.set('Default Welcome Intent', welcome);
     intentMap.set('SearchCourse', SearchCourse);SearchCredits
     intentMap.set('SearchCredits', SearchCredits);
+    intentMap.set('SearchSemesterCredits', SearchSemesterCredits);
     //------------------------------------
     agent.handleRequest(intentMap);         
 });
